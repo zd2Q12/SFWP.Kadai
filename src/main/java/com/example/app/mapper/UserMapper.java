121687fa79d432c601a62c3fd3892ba7bd58f1d7
suzuki <ajcnskdjcnoswn@gmail.com>
2024-11-21 09:03:29 +0900

    引数を受け取る

--- src/main/java/com/example/app/mapper/UserMapper.java
+++ src/main/java/com/example/app/mapper/UserMapper.java
@@ -14,7 +14,7 @@ public interface UserMapper {
 
 	//個別取得　（Read）
 	User getUserById(int id);//IDに対応するUserを取得・表示する
-	User getUserByUserNameAndPassword(String userName);
+	User getUserByUserNameAndPassword(String userName, String password);
 //追加（Create）
 	void addUser(User user);//戻り値がないので、新規ユーザーをDBに格納するのみ
 	
