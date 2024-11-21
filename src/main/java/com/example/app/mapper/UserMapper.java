5913fa572cf658f2b361d78765fddc663fa00b4f
suzuki <ajcnskdjcnoswn@gmail.com>
2024-11-21 09:00:30 +0900

    メソッド名の変更

--- src/main/java/com/example/app/mapper/UserMapper.java
+++ src/main/java/com/example/app/mapper/UserMapper.java
@@ -11,12 +11,10 @@ public interface UserMapper {
 //CRUD処理
 	//管理者用
 	List<User> selectAll();
-	
-	
-	
+
 	//個別取得　（Read）
 	User getUserById(int id);//IDに対応するUserを取得・表示する
-	User getUserByName(String userName);
+	User getUserByUserNameAndPassword(String userName);
 //追加（Create）
 	void addUser(User user);//戻り値がないので、新規ユーザーをDBに格納するのみ
 	
